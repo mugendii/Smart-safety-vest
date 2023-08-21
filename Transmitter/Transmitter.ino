3f4d059644baea9d50bfc32883d75f3ecf02cebd
Mugi  Mugendi <mugiendi318@gmail.com>
2023-08-22 02:09:53 +0300

    Create Transmitter.ino

---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,113 @@
+/* 
+This is the code that will be uploaded on the esp32 board on the transmitter controller
+*/
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x3F , 16, 2);
+// variables
+const int rightButtonPin = 2;  // the number of the right pushbutton pin
+const int leftButtonPin = 2;  // the number of the left pushbutton pin
+
+// variables will change:
+int rightButtonState = 0;  // variable for reading the pushbutton status
+int leftButtonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(2 , 1);
+  lcd.print('Hello rider');
+
+  pinMode(rightButtonPin, INPUT);
+  pinMode(leftButtonPin, INPUT);
+
+}
+
+  void operationMode(){
+ // read the state of the pushbutton value:
+  rightButtonState = digitalRead(rightButtonPin);
+  leftButtonState = digitalRead(leftButtonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (rightButtonState == HIGH) 
+  {
+    // initiate right turn
+    rightTurn();
+  } 
+  else {
+    // turn off:
+    
+  }
+  if (leftButtonState == HIGH)
+  {
+    // initiate left turn
+    leftTurn();
+  } 
+  else {
+    // turn off:
+    
+  }
+
+  }
+
+
+void rightTurn(){
+  /*code to send signal through esp now to blink the leds
+  void SendToReceiver(){
+
+  }*/
+
+  lcd.setCursor(0 , 0);
+  lcd.print('Right turn');
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print('Right turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('Right turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('Right turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('Right turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('Right turn');
+
+
+}
+void leftTurn(){
+  /*code to send signal through esp now to blink the leds
+  void SendToReceiver(){
+
+  }*/
+
+  lcd.setCursor(0 , 0);
+  lcd.print('Left turn');
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print('left turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('left turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('left turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('left turn');
+  lcd.clear();
+  delay(1000);
+  lcd.print('left turn');
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
