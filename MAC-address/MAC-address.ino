83fe014785d6b5ed5d13540c9f9954c9a8263843
Mugi  Mugendi <mugiendi318@gmail.com>
2023-08-27 10:17:30 +0300

    Create MAC-address.ino

---
+++ MAC-address/MAC-address.ino
@@ -0,0 +1,17 @@
+
+#include "WiFi.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //put esp32 intostation mode
+  WiFi.mode(WIFI_MODE_STA);
+  //print MAC address
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
