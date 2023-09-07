93da4d6377abf844f4e5bd3e03ffa466d02c5852
Mugi  Mugendi <mugiendi318@gmail.com>
2023-09-07 15:58:28 +0300

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -18,8 +18,8 @@ const int rightButtonPin = 2;  // the number of the right pushbutton pin
 const int leftButtonPin = 2;  // the number of the left pushbutton pin
 
 // variables will change:
-int rightButtonState = 0;  // variable for reading the pushbutton status
-int leftButtonState = 0;  // variable for reading the pushbutton status
+bool rightButtonState = false;  // variable for reading the pushbutton status
+bool leftButtonState = false;  // variable for reading the pushbutton status
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,35 +28,30 @@ void setup() {
   lcd.setCursor(2 , 1);
   lcd.print('Hello rider');
 
-  pinMode(rightButtonPin, INPUT);
-  pinMode(leftButtonPin, INPUT);
+  pinMode(rightButtonPin, INPUT_PULLUP);
+  pinMode(leftButtonPin, INPUT_PULLUP);
+
+  //initialize ESP_NOW
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Error adding peer");
+    return;
+  }
 
 }
 
   void operationMode(){
- // read the state of the pushbutton value:
-  rightButtonState = digitalRead(rightButtonPin);
-  leftButtonState = digitalRead(leftButtonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (rightButtonState == HIGH) 
-  {
-    // initiate right turn
-    rightTurn();
-  } 
-  else {
-    // turn off:
-    
-  }
-  if (leftButtonState == HIGH)
-  {
-    // initiate left turn
-    leftTurn();
-  } 
-  else {
-    // turn off:
-    
-  }
+ 
 
   }
 
@@ -66,6 +61,22 @@ void rightTurn(){
   void SendToReceiver(){
 
   }*/
+  //create message
+  uint8_t messageright[1];
+  messageright[0] = rightButtonState;
+
+    // Send the message
+  esp_err_t resultright = esp_now_send(broadcastAddress, messageright, sizeof(messageright));
+
+  if (resultright == ESP_OK) {
+    Serial.println("Message sent successfully");
+  } else {
+    Serial.println("Error sending message");
+  }
+
+  delay(1000);
+
+
 
   lcd.setCursor(0 , 0);
   lcd.print('Right turn');
@@ -94,6 +105,24 @@ void leftTurn(){
 
   }*/
 
+   //create message
+  uint8_t messageleft[1];
+  messageleft[0] = leftButtonState;
+
+    // Send the message
+  esp_err_t resultleft = esp_now_send(broadcastAddress, messageleft, sizeof(messageleft));
+
+  if (resultleft == ESP_OK) {
+    Serial.println("Message sent successfully");
+  } else {
+    Serial.println("Error sending message");
+  }
+
+  delay(1000);
+
+
+
+
   lcd.setCursor(0 , 0);
   lcd.print('Left turn');
   delay(1000);
@@ -118,5 +147,20 @@ void leftTurn(){
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // read the state of the pushbutton value:
+  rightButtonState = digitalRead(rightButtonPin);
+  leftButtonState = digitalRead(leftButtonPin);
 
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (rightButtonState == HIGH) {
+    // initiate right turn
+    rightTurn();
+    return;
+  } 
+    
+  if (leftButtonState == HIGH){
+    // initiate left turn
+    leftTurn();
+    return;
+  } 
 }
