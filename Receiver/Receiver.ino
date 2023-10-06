2b2045a55c071147742e6241d8305f8186fb654a
Mugi  Mugendi <mugiendi318@gmail.com>
2023-10-06 09:21:24 +0300

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -5,7 +5,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <fastLED.h>
 #include <esp_now.h>
-#include <WiFi.h>
+include <WiFi.h>
 
 
 // Replace with the MAC address of the transmitter ESP32
@@ -15,7 +15,9 @@ const int LED_PINRIGHT = 13;  // Pin connected to the LED
 const int LED_PINLEFT = 12;
 
 // Pin to use to send signals to WS2812B
-#define LEDSTRIP_PIN 6
+#define REDLEDSTRIP_PIN 25
+#define BLUELEDSTRIP_PIN 26
+#define GREENLEDSTRIP_PIN 27
 
 // Number of WS2812B LEDs attached to the Arduino
 #define LED_COUNT 12
@@ -50,10 +52,7 @@ void setup() {
   esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len) {
     if (len == 1) {
       if (data[0] == HIGH) {
-        digitalWrite(LED_PIN_1, HIGH);
-        delay(500);
-        digitalWrite(LED_PIN_1, LOW);
-        delay(500);// Turn on the first LED
+        digitalWrite(LED_PIN_1, HIGH);  // Turn on the first LED
       } else {
         digitalWrite(LED_PIN_1, LOW);   // Turn off the first LED
       }
@@ -61,10 +60,7 @@ void setup() {
     } 
     if (len == 2) {
       if (data[1] == HIGH) {
-        digitalWrite(LED_PIN_2, HIGH);
-        delay(500);
-        digitalWrite(LED_PIN_2, LOW);
-        delay(500);// Turn on the second LED
+        digitalWrite(LED_PIN_2, HIGH);  // Turn on the second LED
       } else {
         digitalWrite(LED_PIN_2, LOW);   // Turn off the second LED
       }
